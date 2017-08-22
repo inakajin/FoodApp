85e9bd0beda58dabc2de79647983bfd9d160d837
Mike Dickerson <inakajin@gmail.com>
2017-08-22 18:28:22 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 //Define URL to call as global variable
-/*const RECIPE_SEARCH_URL = 'https://food2fork.com/api/search';*/
 const RECIPE_SEARCH_URL = 'https://api.edamam.com/search';
 
 //This function calls the Recipe API
@@ -9,29 +8,19 @@ function getDataFromApi(searchTerm, callback) {
       q: searchTerm,
       app_id: 'cc3ee280',
       app_key: 'e93e9043178ce874680226c6ce8acc49',
-      /*part: 'recipes'*/
     };
   $.getJSON(RECIPE_SEARCH_URL, settings, callback);
 }
 
 const template = {
   item: function(item) {
-    /*console.log(item.recipe.ingredientLines);*/
-   /* var htmlOutput = "";
-    $.each(item); {
-      htmlOutput += 
-      '<div class="inredients">' +
-      '<p>' + item.recipe.ingredientLines + '</p>' +
-      '</div>';
-    }*/
-    /*item.recipe.url = url.replace(/^http:\/\//i, 'https://');*/
     return '<div class ="col-4">' +
               '<div class ="result">' +
                 '<div class="recipelabel">' +
                   '<div class="reclist">' + item.recipe.ingredientLines + '</div><!-- end reclist -->' +
                   '<p class="label">' + item.recipe.label + '</p>' +
                     '<div class="thumbnail">' + 
-                      '<a href='+ item.recipe.url +'"target="_blank">' +
+                      '<a href="'+ item.recipe.url + '" target="_blank">' +
                       '<img src="' + item.recipe.image + '"alt="' + item.recipe.label + '">' +
                       '</a>' +
                     '<div class="recipesource">' +
@@ -56,17 +45,10 @@ function searchSubmit() {
 
 //render results
 function displayRecipeSearchData(data) {
-//  console.log('myitems:'+data);
-//  console.dir(data);
   var results = ' ';
   if (data.hits) {
     data.hits.forEach(function(item) {
-      /*item.recipe.url = url.replace(/^http:\/\//i, 'https://');*/
-      console.log('singleitem:'+item);
- //   console.log('template',template.item);
       results += template.item(item);
-//      var div = "<div>test</div>";
-//      results += div;
     });
   }
   else {
@@ -76,24 +58,15 @@ function displayRecipeSearchData(data) {
 }
 
 //Cleans the url
+//function /*item.recipe.url = url.replace(/^http:\/\//i, 'https://');*/
 
 //hover behavior for ingredients pop-up
-$(function(event) {
-  var moveLeft = 20;
-  var moveDown = 10;
-
-  $('div.recipelabel').hover(function(e) {
-  console.log("*****");
-    $('div.reclist').show();
-  }, function() {
-    $('div.reclist').hide();
+$(function() {
+  searchSubmit();
+  $('body').on('mouseenter','div.recipelabel',function(e) {
+    $(this).children('div.reclist').show();
+  }).on('mouseleave','div.recipelabel',function(e){
+    $(this).children('div.reclist').hide();
   });
-
-  $('div.recipelabel').mousemove(function(e) {
-    $("div.reclist").css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
-  });
-
 });
 
-
-$(searchSubmit);
